3342702fa88021040630c1dca62cab665020de15
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2024-11-20 10:49:39 +0545

    Animation in Login

--- login.php
+++ login.php
@@ -51,12 +51,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <?php endif; ?>
         <form action="login.php" method="post">
             <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" required>
+                <!-- <label for="username">Username</label> -->
+                <input type="text" id="username" name="username" placeholder="Username" required>
             </div>
             <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
+                <!-- <label for="password">Password</label> -->
+                <input type="password" id="password" name="password" placeholder="Password" required>
             </div>
             <button type="submit" class="btn">Login</button>
         </form>
