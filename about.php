a1f2d7e67e02075ffebdd14f8c03d4f42dceafbf
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2024-11-22 18:24:35 +0545

    created structures and required files of homepage

---
+++ about.php
@@ -0,0 +1,4 @@
+<section class="about">
+    <h2>About Sasto E-Pasal</h2>
+    <p>We are dedicated to providing affordable books for everyone.</p>
+</section>
---
+++ homepage.php
@@ -0,0 +1,5 @@
+<section class="homepage">
+    <h2>Featured Books</h2>
+    <p>Explore our collection of best-selling books!</p>
+    <!-- Add dynamic content like book listings here -->
+</section>
