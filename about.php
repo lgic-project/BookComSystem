eaf57b2dd66e3fe0e6e2cb2b137254cc2817c348
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2024-12-03 22:50:15 +0545

    some changes fixed

--- about.php
+++ about.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
+
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/aboutus.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
 
 </head>
 
@@ -32,7 +34,7 @@
                     <span class="job">Developer</span>
                 </div>
                 <div class="media-buttons">
-                    <a href="https://www.facebook.com/profile.php?id=100012898502822" target="_blank" style="background: #4267b2" class="link"> <i class="bx bxl-facebook"></i> </a>
+                    <a href="https://www.facebook.com/profile.php?id=100012898502822" target="_blank" style="background: #4267b2" class="link"> <i class="fas fa-facebook"></i> </a>
                     <a href="https://www.instagram.com/deepak_chhantyal/" target="_blank" style="background: #e1306c" class="link"> <i class="bx bxl-instagram"></i> </a>
                     <a href="https://github.com/deepak4156" target="_blank" style="background: #000" class="link"> <i class="bx bxl-github"></i> </a>
                     <a href="https://www.linkedin.com/in/deepak-chhantyal-4a5ab0165/" target="_blank" style="background: #4267bf" class="link"> <i class="bx bxl-linkedin"></i> </a>
