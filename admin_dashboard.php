f03e3225850a4c1d78521bb816d4dfa1f1bfae6b
roshan adhikari <adksabin779@gmail.com>
2024-12-05 10:00:20 +0545

    admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -86,34 +86,6 @@
 
       <section class="main-course">
         <h1>My courses</h1>
-        <div class="course-box">
-          <ul>
-            <li class="active">In progress</li>
-            <li>explore</li>
-            <li>incoming</li>
-            <li>finished</li>
-          </ul>
-          <div class="course">
-            <div class="box">
-              <h3>HTML</h3>
-              <p>80% - progress</p>
-              <button>continue</button>
-              <i class="fab fa-html5 html"></i>
-            </div>
-            <div class="box">
-              <h3>CSS</h3>
-              <p>50% - progress</p>
-              <button>continue</button>
-              <i class="fab fa-css3-alt css"></i>
-            </div>
-            <div class="box">
-              <h3>JavaScript</h3>
-              <p>30% - progress</p>
-              <button>continue</button>
-              <i class="fab fa-js-square js"></i>
-            </div>
-          </div>
-        </div>
       </section>
     </section>
   </div>
