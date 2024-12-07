a9d7fdec0b29a088bccf6c495097613797d0d5fd
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2024-12-07 10:43:02 +0545

    user-cart

--- user_cart.php
+++ user_cart.php
@@ -2,6 +2,18 @@
 $user_id = 1;
 include_once './connection/config.php';
 
+if(isset($_POST['quantity_added'])){
+    $book_id = $_POST['book_id'];
+    $book_quantity = $_POST['quantity'];
+    $cart_id= $_POST['cart_id'];
+
+    if($stmt = $mysqli->query("UPDATE `cart` SET quantity='$book_quantity' WHERE id='$cart_id'")){
+        header("Location: User_cart?cart=updated");
+        $stmt->close();
+    }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -19,8 +31,8 @@ include_once './connection/config.php';
             $grand_total = 0;
             $cart = $mysqli->query("SELECT * FROM `cart` WHERE user_id = '$user_id'");
             if ($cart->num_rows > 0) {
-                while ($row = $cart->fetch_assoc()) {
-                    $book_details = $mysqli->query("SELECT title,author,price, stock, book_img FROM books WHERE id='$row[product_id]'");
+                while ($cart_row = $cart->fetch_assoc()) {
+                    $book_details = $mysqli->query("SELECT title,author,price, stock, book_img FROM books WHERE id='$cart_row[product_id]'");
                     if($book_details->num_rows>0){
                         $book_row = $book_details->fetch_assoc();
                     }
@@ -32,12 +44,13 @@ include_once './connection/config.php';
                     <p class="price">$<?php echo htmlspecialchars($book_row['price']) ?> </p>
                    
                     <form action="" Method="POST">
-                    <!-- <input type="hidden" name="book_id" value="<?php echo $book_row['id']; ?>">
-                    <input type="hidden" name="book_price" value="<?php echo $book_row['price']; ?>"> -->
-                    <input type="number" name="quantity" value="1" min="1" required>
-                    <button type="submit">Add</button>
+                    <input type="hidden" name="book_id" value="<?php echo $cart_row['product_id'] ?>">
+                    <input type="hidden" name="cart_id" value="<?php echo $cart_row['id'] ?>">
+                    <input type="hidden" name="book_price" value="<?php echo $book_row['price']; ?>">
+                    <input type="number" name="quantity" value="<?php echo $cart_row['quantity']; ?>" min="1" required>
+                    <button type="submit" name="quantity_added">Add</button>
                     </form>
-                    <div class="sub_total">Total: <span>$<?php echo $sub_total= $book_row['price'] * $row['quantity'] ?> </span></div>
+                    <div class="sub_total">Total: <span>$<?php echo $sub_total= $book_row['price'] * $cart_row['quantity'];  ?> </span></div>
                     </div>
                     <?php
                     $grand_total+= $sub_total;
